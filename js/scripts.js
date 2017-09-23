288576e3f3f47afc520e0f7f2e922ba96a0b2837
ninja_koder <grinders@tlen.pl>
2017-09-23 10:21:03 +0200

    Skończony projekt.

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var height= 6,
+    aSide= 2.5,
+    area= 0.5 * height * aSide;
+
+alert('Pieseł mówi, iż pole trójkąta o wysokości: ' + height + ' cm, długości boku: ' + aSide + ' cm, wynosi ' + area + ' cm2.');
