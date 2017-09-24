b2dc2cf1c1d8275caa609a6b6a40c376eec14967
ninja_koder <grinders@tlen.pl>
2017-09-24 08:38:19 +0200

    Estetyka porawiona.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var height= 6,
-    aSide= 2.5,
-    area= 0.5 * height * aSide;
+var height = 6,
+    aSide = 2.5,
+    area = 0.5 * height * aSide;
 
 alert('Pieseł mówi, iż pole trójkąta o wysokości: ' + height + ' cm, długości boku: ' + aSide + ' cm, wynosi ' + area + ' cm2.');
